0f9dc42773c8f653dc9474786ea1d1111f42b326
akato-the-dragon <e-stepanov-ig@yandex.ru>
2025-12-11 18:36:25 +0300

    Add tray and rework styles

--- core/ui/object/main_window.py
+++ core/ui/object/main_window.py
@@ -1,11 +1,12 @@
-from PySide6.QtGui import QAction
-from PySide6.QtCore import QThread
 from typing import Optional
 from core.meta import get_full_version
+from PySide6.QtGui import QAction, QIcon
 from core.service import ExtensionService
 from qframelesswindow import FramelessMainWindow
+from PySide6.QtCore import QThread, QTimer, QEvent
+from core.utility.widget_tools import label_insert_text
 from core.style.style_manager import load_stylesheet_from_file
-from PySide6.QtWidgets import QWidget, QMenuBar, QMenu, QSystemTrayIcon
+from PySide6.QtWidgets import QApplication, QWidget, QMenuBar, QMenu, QSystemTrayIcon
 import webbrowser
 
 from core.ui.layout.main_window import Ui_main_window
@@ -49,20 +50,21 @@ class MainWindow(FramelessMainWindow):
         self._tray = QSystemTrayIcon(self)
 
         self.setup_ui()
+        self.setup_tray()
         self.style_ui()
 
         self.titleBar.raise_()
 
     def get_service_status(self) -> None:
         status = "Online" if self._extension_service.is_service_online() else "Offline"
-        self._ui.action_service_status.setText(status)
+        label_insert_text(self._ui.service_status_action, status)
 
         QTimer.singleShot(5000, self.get_service_status)
     
     def get_extension_status(self) -> None:
         status = self._extension_service.is_extension_online()
         self._title_bar.ui.status_widget.set_status(status)
-        self._ui.action_extension_status.setText("Online" if status else "Offline")
+        label_insert_text(self._ui.extension_status_action, "Online" if status else "Offline")
 
         QTimer.singleShot(5000, self.get_extension_status)
 
@@ -78,19 +80,22 @@ class MainWindow(FramelessMainWindow):
 
     def stop_extension_service(self) -> None:
         self._extension_service.thread().terminate()
+        self._extension_service.thread().wait()
 
     def setup_tray(self) -> None:
-        self._tray.setIcon()
         self._tray.setVisible(True)
+        self._tray.setIcon(QIcon("resources/images/icons/tray.png"))
 
         self._tray_menu = QMenu()
         self._open_app_action = QAction("Открыть")
-        self._open_app_action.triggered.connect()
+        self._open_app_action.triggered.connect(self.show)
         self._tray_menu.addAction(self._open_app_action)
 
         self._close_app_action = QAction("Закрыть")
-        self._close_add_action.triggered.connect()
+        self._close_app_action.triggered.connect(QApplication.quit)
         self._tray_menu.addAction(self._close_app_action)
+
+        self._tray.setContextMenu(self._tray_menu)
 
     def setup_ui(self) -> None:
         self.menuBar().hide()
@@ -113,14 +118,19 @@ class MainWindow(FramelessMainWindow):
         self._ui.github_page_action.triggered.connect(lambda: webbrowser.open("https://github.com/akato-the-dragon/Obscura"))
         self._ui.download_extension_action.triggered.connect(lambda: webbrowser.open("https://bormart.ru:8000/q?f=pun7bgW6VAYWNzIP"))
 
-        self._ui.action_version.setText(get_full_version())
+        label_insert_text(self._ui.action_version, get_full_version())
 
         self.get_service_status()
         self.get_extension_status()
     
     def style_ui(self) -> None:
         load_stylesheet_from_file(self, "resources/styles/main_window.qss")
+        load_stylesheet_from_file(self._tray_menu, "resources/styles/tray_menu.qss")
     
     @property
     def ui(self) -> Ui_main_window:
         return self._ui
+    
+    def closeEvent(self, event: QEvent) -> None:
+        event.ignore()
+        self.hide()
